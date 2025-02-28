b257d023a7f862e8dd0823a726977d0bff08262f
Abhishek Maurya <abhimaurya9850@gmail.com>
2025-02-28 13:37:58 +0530

    succesfully configure setup and depedencies

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages # type: ignore
+
+
+setup(
+
+    name="src",
+    version="0.0.1",
+    author="Abhishek",
+    author_email="abhimaurya9850@gmail.com",
+    packages=find_packages()
+)
